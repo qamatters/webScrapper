170de193efd5248e3d0a67e6a6b43699fe890ca1
Deepak Mathpal <deemathp@publicisgroupe.net>
2019-10-07 18:15:21 +0530

    Added examples

--- wordList.py
+++ wordList.py
@@ -16,30 +16,36 @@ meaning_format.set_font_color('blue')
 words_format = workbook.add_format()
 words_format.set_font_color('green')
 
+# example_format = workbook.add_format()
+# example_format.set_text_wrap()
+
 
 # Write some data headers.
 worksheet.write('A1', 'Words', bold)
 worksheet.write('B1', 'Meanings', bold)
+worksheet.write('C1', 'Example', bold)
 
 res = requests.get('https://www.vocabulary.com/lists/1527292')
 list = bs4.BeautifulSoup(res.text, "html.parser")
 words = list.select('a.word.dynamictext')
 # print(words)
-print('Total words count :' + str(len(words)))
+# print('Total words count :' + str(len(words)))
 
 # Print duplicate words
-duplicateWords = pd.Series(words)[pd.Series(words).duplicated()].values
-print('Total Number of Duplicate words ' + str(len(duplicateWords)))
-print(duplicateWords)
+# duplicateWords = pd.Series(words)[pd.Series(words).duplicated()].values
+# print('Total Number of Duplicate words ' + str(len(duplicateWords)))
+# print(duplicateWords)
 
 meanings = list.select('.definition')
 # print(meanings)
-print('Total Meanings :' + str(len(meanings)))
+# print('Total Meanings :' + str(len(meanings)))
 
 # Print duplicate meanings
-duplicateMeanings = pd.Series(meanings)[pd.Series(meanings).duplicated()].values
-print('Total Number of Duplicate Meanings ' + str(len(duplicateMeanings)))
-print(duplicateMeanings)
+# duplicateMeanings = pd.Series(meanings)[pd.Series(meanings).duplicated()].values
+# print('Total Number of Duplicate Meanings ' + str(len(duplicateMeanings)))
+# print(duplicateMeanings)
+
+examples = list.select('.example')
 
 for i in range(len(words)):
     # print(words[i].getText())
@@ -48,6 +54,11 @@ for i in range(len(words)):
 for j in range(len(meanings)):
     # print(meanings[j].getText())
     worksheet.write(j + 1, 1, meanings[j].getText(), meaning_format)
+
+for k in range(len(examples)):
+    # print(examples[k].getText())
+    worksheet.write(k + 1, 2, examples[k].getText())
+
 
 workbook.close()
 print('Word list created')
